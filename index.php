ce65a42d659d09a56f0a6c451d33c3a945831400
Taras <t.kumpanenko@gmail.com>
2016-06-24 00:00:25 +0300

    view update

--- index.php
+++ index.php
@@ -10,14 +10,26 @@
 <head>
     <meta charset="utf-8" />
     <title>PHP todo APP</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.css"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.js"></script>
+    <style>
+        h1.ui.center.header {
+            margin: 1em 0;
+        }
+    </style>
 </head>
 <body>
-      <h1>test</h1>
-      <!-- Your Semantic UI Code -->
-
-
+    <h1 class="ui center aligned header">
+        TODO 
+    </h1>
+    <div class="ui text container">
+      <div class="ui segments">
+        <div class="ui segment">Content</div>
+        <div class="ui segment">Content</div>
+        <div class="ui segment">Content</div>
+        <div class="ui segment">Content</div>
+      </div>
+    </div>
 </body>
 </html>
